8e0d21afcd488f68839405e07e222beaa8254c27
themarchuerta <huertamarc68@gmail.com>
2024-10-09 03:10:01 -0700

    mm

--- hw1-stoch-compute/questions.md
+++ hw1-stoch-compute/questions.md
@@ -4,20 +4,28 @@ The provided scaffold code runs 10,000 trials of the `1/2*(0.8*0.4 + 0.6)` stoch
 
 Implement the stochastic computing paradigm by filling in the function stubs in the PosStochComputing class. Don't worry about supporting negative numbers just yet.
 
-- `to_stoch` : convert a value in [0,1] to a stochastic bitstream.
-- `from_stoch`: convert a stochastic bitstream to a numerical value
-- `stoch_add`: perform scaled addition over two stochastic bitstreams.
-- `stoch_mul`: perform multiplication over two stochastic bitstreams.
+<!-- - `to_stoch` : convert a value in [0,1] to a stochastic bitstream. -->
+<!-- - `from_stoch`: convert a stochastic bitstream to a numerical value -->
+<!-- - `stoch_add`: perform scaled addition over two stochastic bitstreams. -->
+<!-- - `stoch_mul`: perform multiplication over two stochastic bitstreams. -->
 
 Execute the stochastic computation for bitstream lengths 10, 100, and 1000 with `stochastic.py`, and answer the following questions.
 
 Q1. How does the mean change with increasing bitstream length? How does the variance change?
 
+The mean actually stay surprisingly accurate! The variance just gets vastly smaller as bitstream length increases.
+
 Q2. What is the smallest representable numeric value in 1000-bit stochastic bitstream? What happens when you try to generate a bitstream for this value -- do the bitstream values converge to the desired value?
+
+1/1000 is the smallest. Yea they'll still form a good distrubution except since there's no space for variance below 0/1000, it will also have a bunch of results be 0 almost as much as it is 1/1000.
 
 Q3. Using what you leaned from the analysis in (Q2), design a stochastic computation that produces an incorrect result for bitstreams with a length n=1000. You must accomplish this with stochastic operations, all values must be >= 0.1 and every constant must be a uniquely generated bitstream. What is the smallest bitstream length for the stochastic computation you come up with to produce the correct result?
 
+Ok so if I try to represent 1/1000, about half the time it will be 0. So if I multiply 0.1 * 0.1 * 0.1 I get 1/1000. So that's pretty inaccurate. Having a bitstream of length 2000 would produce a more accurate result. Any value between 0 and 1/1000 that I try to get out will give me just 0 a lot of the time.
+
 Q4. What stochastic bitstream length L do we need to represent a value V, assuming V is in [0,1] and V != 0? Write the equation.
+
+It'd just be the precison of the lowest decimal place number and use that to divide 1. So .00453 would be 1/0.00003.
 
 #### Part X: Non-Idealities Stochastic Computing [2 pt/question, 4 points]
 
@@ -31,9 +39,15 @@ Fill in the `apply_bitshift` and `apply_bitflip` functions in the stochastic com
 
 Q1. What happens to the computational results when you introduce a per-bit bit-flip error probability of 0.0001? What happens when the per-bit bit flip error probability is 0.1?
 
+Literally nothing almost-- it is super immune to these errors espcially with such a low probability. If it was 0.1 then it would highly more affect the computation and create high variance and shift the computational mean up or down 0.1.
+
 Q2. What happens to the computational results when you introduce a per-bit bit-shift error probability of 0.0001? What happens when the per-bit bit shift error probability is 0.1?
 
+For both situations nothing happened-- it is much more resilient to this as it still keeps a lot of its own data, it just shfits down 1 number and copies one one at a index which randomly further samples the own distrubtion of the vector so it stays the same because of that too.
+
 Q3. In summary, is the computation affected by these non-idealities? Do you see any changes in behavior as the bitstream grows?
+
+Not really that much, especially not as much as classical computing. The bitflip one is more affected but still not that much relatively. As the bitstream grows it won't make a difference for any error, the expected value and error would remain the same.
 
 #### Part Y: Statically Analyzing Stochastic Computations [2 pt/question, 8 points]
 
@@ -58,7 +72,11 @@ We will use this static analysis to figure out what stochastic bistream length t
 
 Q1. Describe how your precision analysis works. Specifically, how do you propagate the precisions through the entire computation? How do you determine the final size?
 
+I keep a private variable keeping track of the state of precision and updating based on the operation.
+
 Q2. What bitstream length did your analysis return?
+
+
 
 Q3. How did the random executions perform when parametrized with the analyzer-selected bitstream length?
 
--- hw1-stoch-compute/stochastic.py
+++ hw1-stoch-compute/stochastic.py
@@ -10,64 +10,92 @@ class PosStochasticComputing:
     def apply_bitshift(cls, bitstream):
         if not PosStochasticComputing.APPLY_SHIFTS:
             return bitstream
-
-        raise Exception("apply the bitshift error to the bitstream with probability 0.0001")
+        n = len(bitstream)
+        shifted_bitstream = np.copy(bitstream)
+        for i in range(n):
+            if np.random.rand() < 0.1:
+                for j in range (i,n-1):
+                    # Shift right just like the slides and example (unsure if we should account for a shift left)
+                    shifted_bitstream[j+1] = bitstream[j]
+        return shifted_bitstream
+        # raise Exception("apply the bitshift error to the bitstream with probability 0.0001")
 
     @classmethod
     def apply_bitflip(self, bitstream):
         if not PosStochasticComputing.APPLY_FLIPS:
             return bitstream
-
-        raise Exception("apply the to the bitstream with probability 0.0001")
-
-
+        n = len(bitstream)
+        flipped_bitstream = np.copy(bitstream)
+        for i in range(n):
+            if np.random.rand() < 0.0001:
+                flipped_bitstream[i] = np.random.rand()
+        return flipped_bitstream
+        # raise Exception("apply the to the bitstream with probability 0.0001")
 
     @classmethod
     def to_stoch(cls, prob, nbits):
         assert(prob <= 1.0 and prob >= 0.0)
-        raise Exception("convert a decimal value in [0,1] to an <nbit> length bitstream.")
+        bitstream = np.random.rand(nbits) < prob
+        return bitstream.astype(int)
+        # raise Exception("convert a decimal value in [0,1] to an <nbit> length bitstream.")
 
     @classmethod
     def stoch_add(cls, bitstream, bitstream2):
         assert(len(bitstream) == len(bitstream2))
-        raise Exception("add two stochastic bitstreams together")
+        n = len(bitstream)
+        # randomly choose
+        result = [bitstream[i] if np.random.rand() < 0.5 else bitstream2[i] for i in range(n)]
+        return result
+        # raise Exception("add two stochastic bitstreams together")
 
     @classmethod
     def stoch_mul(cls, bitstream, bitstream2):
         assert(len(bitstream) == len(bitstream2))
-        raise Exception("multiply two stochastic bitstreams together")
+        # & op
+        result = np.logical_and(bitstream, bitstream2).astype(int)
+        return result
+        # raise Exception("multiply two stochastic bitstreams together")
 
     @classmethod
     def from_stoch(cls, result):
-        raise Exception("convert a stochastic bitstream to a numerical value")
+        return np.mean(result)
+        # raise Exception("convert a stochastic bitstream to a numerical value")
 
 class StochasticComputingStaticAnalysis:
 
     def __init__(self):
+        self.current_precision = None
         pass
 
     def req_length(self, smallest_value):
-        raise Exception("figure out the smallest bitstream length necessary represent the input decimal value. This is also called the precision.")
+        return int(math.ceil(1 / smallest_value))
+        # raise Exception("figure out the smallest bitstream length necessary represent the input decimal value. This is also called the precision.")
 
     def stoch_var(self, prec):
-        raise Exception("update static analysis -- the expression contains a variable with precision <prec>.")
-        result_prec = None
-        return result_prec
-
+        # raise Exception("update static analysis -- the expression contains a variable with precision <prec>.")
+        if self.current_precision is None:
+            self.current_precision = prec
+        else:
+            self.current_precision = min(self.current_precision, prec)
+        return self.current_precision
 
     def stoch_add(self, prec1, prec2):
-        raise Exception("update static analysis -- the expression adds together two bitstreams with precisions <prec1> and <prec2> respectively.")
-        result_prec = None
+        # raise Exception("update static analysis -- the expression adds together two bitstreams with precisions <prec1> and <prec2> respectively.")
+        result_prec = (prec1 + prec2) / 2.0
+        print("HELLLI")
+        print(result_prec)
+        self.current_precision = result_prec
         return result_prec
 
-
     def stoch_mul(self, prec1, prec2):
-        raise Exception("update static analysis -- the expression multiplies together two bitstreams with precisions <prec1> and <prec2> respectively.")
-        result_prec = None
-        return res_prec
+        # raise Exception("update static analysis -- the expression multiplies together two bitstreams with precisions <prec1> and <prec2> respectively.")
+        result_prec = prec1 * prec2
+        self.current_precision = result_prec
+        return result_prec
 
     def get_size(self):
-        raise Exception("get minimum bitstream length required by computation.")
+        # raise Exception("get minimum bitstream length required by computation.")
+        return self.req_length(self.current_precision)
 
 
 
@@ -91,7 +119,6 @@ def run_stochastic_computation(lambd, ntrials, visualize=True, summary=True):
 
 
 
-
 def PART_A_example_computation(bitstream_len):
     # expression: 1/2*(0.8 * 0.4 + 0.6)
     reference_value = 1/2*(0.8 * 0.4 + 0.6)
@@ -102,6 +129,51 @@ def PART_A_example_computation(bitstream_len):
     result = PosStochasticComputing.stoch_add(tmp, y)
     return reference_value, PosStochasticComputing.from_stoch(result)
 
+
+def PART_A_example_computation_q3(bitstream_len):
+    reference_value = 0.1 * 0.1 * 0.1 * 0.9 * 0.9 * 0.9
+    w = PosStochasticComputing.to_stoch(0.1, bitstream_len)
+    x = PosStochasticComputing.to_stoch(0.1, bitstream_len)
+    z = PosStochasticComputing.to_stoch(0.1, bitstream_len)
+    a = PosStochasticComputing.to_stoch(0.9, bitstream_len)
+    b = PosStochasticComputing.to_stoch(0.9, bitstream_len)
+    c = PosStochasticComputing.to_stoch(0.9, bitstream_len)
+    final = PosStochasticComputing.stoch_mul(w, x)
+    result = PosStochasticComputing.stoch_mul(final, z)
+    final2 = PosStochasticComputing.stoch_mul(result, a)
+    final3 = PosStochasticComputing.stoch_mul(final2, b)
+    final4 = PosStochasticComputing.stoch_mul(final3, c)
+    return reference_value, PosStochasticComputing.from_stoch(final4)
+
+def PART_X_flip_computation(bitstream_len):
+    # expression: 1/2*(0.8 * 0.4 + 0.6)
+    reference_value = 1/2*(0.8 * 0.4 + 0.6)
+    w = PosStochasticComputing.to_stoch(0.8, bitstream_len)
+    w = PosStochasticComputing.apply_bitflip(w)
+    x = PosStochasticComputing.to_stoch(0.4, bitstream_len)
+    x = PosStochasticComputing.apply_bitflip(x)
+    y = PosStochasticComputing.to_stoch(0.6, bitstream_len)
+    y = PosStochasticComputing.apply_bitflip(y)
+    tmp = PosStochasticComputing.stoch_mul(x, w)
+    tmp = PosStochasticComputing.apply_bitflip(tmp)
+    result = PosStochasticComputing.stoch_add(tmp, y)
+    result = PosStochasticComputing.apply_bitflip(result)
+    return reference_value, PosStochasticComputing.from_stoch(result)
+
+def PART_X_shift_computation(bitstream_len):
+    # expression: 1/2*(0.8 * 0.4 + 0.6)
+    reference_value = 1/2*(0.8 * 0.4 + 0.6)
+    w = PosStochasticComputing.to_stoch(0.8, bitstream_len)
+    w = PosStochasticComputing.apply_bitshift(w)
+    x = PosStochasticComputing.to_stoch(0.4, bitstream_len)
+    x = PosStochasticComputing.apply_bitshift(x)
+    y = PosStochasticComputing.to_stoch(0.6, bitstream_len)
+    y = PosStochasticComputing.apply_bitshift(y)
+    tmp = PosStochasticComputing.stoch_mul(x, w)
+    tmp = PosStochasticComputing.apply_bitshift(tmp)
+    result = PosStochasticComputing.stoch_add(tmp, y)
+    result = PosStochasticComputing.apply_bitshift(result)
+    return reference_value, PosStochasticComputing.from_stoch(result)
 
 def PART_Y_analyze_wxb_function(precs):
     # 1/2*(w*x + b)
@@ -167,20 +239,30 @@ def PART_Z_execute_rng_efficient_computation(value,N,save_rngs=True):
 
 print("---- part a: effect of length on stochastic computation ---")
 ntrials = 10000
-run_stochastic_computation(lambda : PART_A_example_computation(bitstream_len=10), ntrials)
-run_stochastic_computation(lambda : PART_A_example_computation(bitstream_len=100), ntrials)
-run_stochastic_computation(lambda : PART_A_example_computation(bitstream_len=1000), ntrials)
+# run_stochastic_computation(lambda : PART_A_example_computation(bitstream_len=10), ntrials)
+# run_stochastic_computation(lambda : PART_A_example_computation(bitstream_len=100), ntrials)
+# run_stochastic_computation(lambda : PART_A_example_computation(bitstream_len=1000), ntrials)
+######## Q3 of Part A
+# run_stochastic_computation(lambda : PART_A_example_computation_q3(bitstream_len=1000), ntrials)
 
+## Part X
+# run_stochastic_computation(lambda : PART_X_flip_computation(bitstream_len=10), ntrials)
+# run_stochastic_computation(lambda : PART_X_flip_computation(bitstream_len=100), ntrials)
+# run_stochastic_computation(lambda : PART_X_flip_computation(bitstream_len=1000), ntrials)
+
+# run_stochastic_computation(lambda : PART_X_shift_computation(bitstream_len=10), ntrials)
+# run_stochastic_computation(lambda : PART_X_shift_computation(bitstream_len=100), ntrials)
+# run_stochastic_computation(lambda : PART_X_shift_computation(bitstream_len=1000), ntrials)
 
 # Part X, introduce non-idealities
 PosStochasticComputing.APPLY_FLIPS = True
 PosStochasticComputing.APPLY_SHIFTS =False
 print("---- part x: effect of bit flips ---")
-run_stochastic_computation(lambda : PART_A_example_computation(bitstream_len=1000), ntrials)
+# run_stochastic_computation(lambda : PART_A_example_computation(bitstream_len=1000), ntrials)
 PosStochasticComputing.APPLY_FLIPS = False
 PosStochasticComputing.APPLY_SHIFTS = True
 print("---- part x: effect of bit shifts ---")
-run_stochastic_computation(lambda : PART_A_example_computation(bitstream_len=1000), ntrials)
+# run_stochastic_computation(lambda : PART_A_example_computation(bitstream_len=1000), ntrials)
 PosStochasticComputing.APPLY_FLIPS = False
 PosStochasticComputing.APPLY_SHIFTS =False
 
@@ -190,11 +272,11 @@ print("---- part y: apply static analysis ---")
 PART_Y_test_analysis()
 
 # Part Z, resource efficent rng generation
-print("---- part z: one-rng optimization ---")
-for _ in range(5):
-    v = round(np.random.uniform(),1)
-    print(f"x = {v}")
-    print("running with save_rngs disabled")
-    run_stochastic_computation(lambda : PART_Z_execute_rng_efficient_computation(value=v, N=1000, save_rngs=False), ntrials)
-    print("running with save_rngs enabled")
-    run_stochastic_computation(lambda : PART_Z_execute_rng_efficient_computation(value=v, N=1000, save_rngs=True), ntrials)
+# print("---- part z: one-rng optimization ---")
+# for _ in range(5):
+#     v = round(np.random.uniform(),1)
+#     print(f"x = {v}")
+#     print("running with save_rngs disabled")
+#     run_stochastic_computation(lambda : PART_Z_execute_rng_efficient_computation(value=v, N=1000, save_rngs=False), ntrials)
+#     print("running with save_rngs enabled")
+#     run_stochastic_computation(lambda : PART_Z_execute_rng_efficient_computation(value=v, N=1000, save_rngs=True), ntrials)
